046dc44dfce6cd1b22bc7d003c2324cbaf7c2462
nikolina1996 <nikolinabucan1996@gmail.com>
2025-05-12 13:33:33 +0200

    direktori

--- src/pages/Company.js
+++ src/pages/Company.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import MissionCard from '../components/MissionCard';
+import { useLanguage } from '../components/LanguageContext';
 
 const Company = () => {
   const location = useLocation();
@@ -10,6 +11,7 @@ const Company = () => {
   const [triggeredCards, setTriggeredCards] = useState([false, false, false]);
   const [triggeredDirectors, setTriggeredDirectors] = useState([false, false, false, false]);
   const [triggeredCompanyText, setTriggeredCompanyText] = useState(false);
+  const { texts } = useLanguage();
 
   useEffect(() => {
     if (location.state && location.state.scrollTo) {
@@ -95,23 +97,23 @@ const Company = () => {
 
   const direktoriData = [
     {
-      ime: "Vlado Delić",
-      opis: `Dr Vlado Delić (1964) je redovni profesor na Fakultetu tehničkih nauka Univerziteta u Novom Sadu i rukovodilac je na nekoliko razvojno-istraživačkih projekata na ovom fakultetu. Uža oblast rada profesora Delića je akustika, audio inženjerstvo i obrada signala, kao i razvoj govornih tehnologija. Okupio je tim izuzetnih mladih stručnjaka i sa njima izgradio poziciju regionalnog lidera u razvoju i primeni govornih tehnologija. Autor je i koautor više knjiga, 4 patenta i 10 tehničkih rešenja, kao i više od 250 naučnih radova objavljenih u naučnim časopisima i na konferencijama.`,
+      ime: [texts.direktori.direktor1.ime],
+      opis: [texts.direktori.direktor1.biografija],
       slika: "images/vlado.jpg"
     },
     {
-      ime: "Darko Pekar",
-      opis: `Dr Darko Pekar (1972), diplomirao je na Fakultetu tehničkih nauka Univerziteta u Novom Sadu. Jedan je od vodećih eksperata uspešnog razvojnog tima iz oblasti govornih tehnologija na FTN, u kojem je stekao bogato iskustvo u razvoju aplikacija, kao i u menadžmentu naučno-tehnoloških projekata. Od 2003. godine je na poziciji direktora AlfaNuma, i u saradnji sa timom sa FTN upravlja zajedničkim radom i razvojem aplikacija iz oblasti govornih tehnologija. Autor je nekoliko uspešnih proizvoda i usluga zasnovanih na govornim tehnologijama. Iako je fokusiran na praktičan razvoj i izlazak na tržište ASR i TTS proizvoda, ima i bogatu naučnu produkciju - objavio je preko 70 radova na konferencijama i u naučnim časopisima, preko 20 tehničkih rešenja i 5 patenata.`,
+      ime: [texts.direktori.direktor2.ime],
+      opis: [texts.direktori.direktor2.biografija],
       slika: "images/darko.png"
     },
     {
-      ime: "Milan Sečujski",
-      opis: `Dr Milan Sečujski (1975) je vanredni profesor i istraživač na Fakultetu tehničkih nauka u Novom Sadu. Bavi se razvojem govornih tehnologija, posebno sa lingvističkog aspekta. Već više od 15 godina predvodi istraživačko-razvojni tim na FTN i u AlfaNumu u oblasti sinteze govora, a radi i na razvoju govorno-jezičkih resursa na koje se oslanjaju brojna razvijena rešenja u oblasti govornih tehnologija. Njegovo poznavanje fonetike, morfologije, sintakse i prozodije južnoslovenskih jezika predstavlja važan oslonac tima sa FTN. Autor je i koautor preko 100 radova objavljenih u naučnim časopisima i na konferencijama, uključujući i 10 realizovanih tehničkih rešenja. Dobitnik je prestižne Pupinove nagrade Matice srpske za svoj magistarski rad u oblasti sinteze govora na srpskom jeziku.`,
+      ime: [texts.direktori.direktor3.ime],
+      opis: [texts.direktori.direktor3.biografija],
       slika: "images/milan.png"
     },
     {
-      ime: "Goran Đaković",
-      opis: `Goran Đaković (1961) je po završetku američke srednje škole u Hagu (Holandija) upisao Elektrotehnički fakultet Univerziteta u Beogradu. Kompaniju Saga osnovao je 1989. godine, koja je pod njegovim vođstvom već skoro čitavu deceniju sistem integrator broj jedan u Srbiji (prema zvaničnim finansijskim izveštajima). Gospodin Đaković poseduje dugogodišnje iskustvo u poslovanju sa istaknutim multinacionalnim kompanijama i sarađuje sa rukovodiocima najvišeg nivoa, kao i sa vladinim i političkim zvaničnicima. Član je Srpske asocijacije menadžera, predsednik je Upravnog odbora Udruženja informatičke delatnosti Privredne komore Beograda, član je Upravnog odbora Privredne komore Beograda i Privredne komore Srbije, kao i mnogih drugih poslovnih i stručnih udruženja.`,
+      ime: [texts.direktori.direktor4.ime],
+      opis: [texts.direktori.direktor4.biografija],
       slika: "images/goran.png"
     }
   ];
@@ -119,61 +121,61 @@ const Company = () => {
   return (
     <div className="company-container">
       <div className="naslovi" id="naslovKompanija">
-        <h4>Profil kompanije</h4>
+        <h4>{texts.kompanija.naslov}</h4>
       </div>
 
       <div
         className={`o-kompaniji fade-section ${triggeredCompanyText ? 'visible' : ''}`}
         ref={kompanijaRef}
       >
-        <p> AlfaNum je inovativno preduzeće koje okuplja tim eksperata koji od 2003. godine nastoji da približi svet savremenih govornih tehnologija širokom krugu korisnika. <br/> <br/>
+        <p> {texts.kompanija.tekst1} <br/> <br/>
 
-U dosadašnjem periodu realizovane su softverske komponente za visoko kvalitetno prepoznavanje i sintezu govora na srpskom, hrvatskom i crnogorskom jeziku. U razvoju ovih govornih tehnologija, koje su inicijalno bile namenjene slepima i slabovidima, AlfaNum je prvi u regionu. Više informacija o našim komponentama možete naći na stranama o proizvodima i uslugama, a kako one rade, možete videti kroz demonstracije. <br/><br/>
+        {texts.kompanija.tekst2} <br/><br/>
 
-Pored govornih tehnologija, AlfaNum se bavi razvojem i implementacijom proizvoda za osobe sa invaliditetom (OSI), kao i pozivnih centara, informacionih centara i baza podataka. Ako razmišljate o unapređenju svog sistema korišćenjem savremenih govornih tehnologija ili vam je potrebna neka druga stručna usluga, slobodno nam se obratite. Poverenje koje nam ukažete ispoštovaćemo u potpunosti.<br/><br/>
+{texts.kompanija.tekst3}<br/><br/>
 
-Napredak računarske tehnologije u poslednjih desetak godina omogućio je da i obični PC računari dostignu potrebnu efikasnost za uspešno prepoznavanje i sintezu govora. Pored toga, njihova relativno niska cena učinila je prednosti govornih tehnologija dostupnim sve većem broju korisnika. Specifičnosti južnoslovenskih jezika su velike, te je razvoj govornih tehnologija za svaki od njih vrlo izazovan zadatak. Pored toga, potencijalna tržišta nisu velika, što je dodatni razlog iz kog se nijedna svetska kompanija donedavno nije odlučila da krene u razvoj prepoznavanja i sintezu govora na srpskom i njemu srodnim jezicima. AlfaNum je na vreme prepoznao ovu stratešku prednost, krenuo u sopstveni razvoj i ostvarivanje sledećih osnovnih ciljeva i aktivnosti: <br/>
+{texts.kompanija.tekst4} <br/>
 
 <ul>
-          <li>Razvoj visoko kvalitetnog fleksibilnog sintetizatora govora na osnovu teksta (TTS)</li>
-          <li>Razvoj automatskog prepoznavanja govora (ASR) na velikim rečnicima</li>
-          <li>Razvoj sistema za konverziju govora</li>
-          <li>Razvoj modula za obradu prirodnog jezika uključujući i upravljanje dijalogom</li>
-          <li>Primene razvijenih govornih tehnologija u zemljama zapadnog Balkana:
+          <li>{texts.kompanija.li1}</li>
+          <li>{texts.kompanija.li2}</li>
+          <li>{texts.kompanija.li3}</li>
+          <li>{texts.kompanija.li4}</li>
+          <li>{texts.kompanija.li5}
             <ul>
-              <li>u multimodalnim sistemima za dijalog čovek-mašina (interaktivni govorni sistemi, pametni telefoni, pametne kuće,...)</li>
-              <li>u aplikacijama kao što su: čitanje teksta, diktiranje teksta, transkripcija govora</li>
-              <li>kao asistivne tehnologije: pomagala za osobe sa oštećenjem vida, govora ili sluha.</li>
+              <li>{texts.kompanija.li51}</li>
+              <li>{texts.kompanija.li52}</li>
+              <li>{texts.kompanija.li53}</li>
             </ul>
           </li>
         </ul>
 
-<b>Naučno-istraživačkim radom u okviru AlfaNum tima bavi se grupa nastavnika i saradnika Katedre za telekomunikacije i obradu signala Fakulteta tehničkih nauka u Novom Sadu. Više detalja o tome možete naći na stranama projekta AlfaNum.</b> </p>
+<b>{texts.kompanija.bold}</b> </p>
       </div>
 
       <div className="misijaVizijaRezultati" ref={sectionRef}>
         <MissionCard
           image="images/misija.png"
-          title="Misija"
-          text="Naša misija je da proizvedemo visoko kvalitetna, sigurna i inovativna IT rešenja i servise za klijente u raznim domenima."
+          title={texts.kartice.misija.naslov}
+          text={texts.kartice.misija.tekst}
           visible={triggeredCards[0]}
         />
         <MissionCard
           image="images/vizija.png"
-          title="Vizija"
-          text="Naša vizija je da ostanemo lider u regionu po pitanju govornih tehnologija za srpski i srodne jezike, neprestano unapređujući svoje proizvode i usluge."
+          title={texts.kartice.vizija.naslov}
+          text={texts.kartice.vizija.tekst}
           visible={triggeredCards[1]}
         />
         <MissionCard
           image="images/rezultati.png"
-          title="Rezultati"
-          text="O našim rezultatima pitajte naše klijente - od pojedinaca i malih preduzeća do državnih institucija."
+          title={texts.kartice.rezultati.naslov}
+          text={texts.kartice.rezultati.tekst}
           visible={triggeredCards[2]}
         />
       </div>
 
       <div className="naslovi" id="naslovProizvod">
-        <h4>Bord direktora</h4>
+        <h4>{texts.direktori.naslov}</h4>
       </div>
 
       <div className="direktori" ref={direktoriRef}>
